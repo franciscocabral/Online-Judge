f45b196406aa35c5aa8cb70347ca9be7844f67eb
kiko5 <kiko5@localhost.localdomain>
2016-08-18 14:31:56 -0300

    1522

--- maratona2016.1/src/br/ufs/uri/u1522/Main.java
+++ maratona2016.1/src/br/ufs/uri/u1522/Main.java
@@ -6,6 +6,7 @@
 package br.ufs.uri.u1522;
 
 import java.util.Scanner;
+import java.util.Stack;
 
 /**
  *
@@ -13,21 +14,120 @@ import java.util.Scanner;
  */
 public class Main {
 
+    public static Stack<Integer> pilha1 = new Stack<Integer>();
+    public static Stack<Integer> pilha2 = new Stack<Integer>();
+    public static Stack<Integer> pilha3 = new Stack<Integer>();
+
+    public static int resolucao() {
+        //Caso base
+        if (pilha1.empty() && pilha2.empty() && pilha3.empty()) {
+            return 0;
+        }
+        if ((pilha1.empty() && pilha2.empty())
+                || (pilha1.empty() && pilha3.empty())
+                || (pilha2.empty() && pilha3.empty())) {
+            return 1;
+        }
+
+        if (pilha1.empty()) {
+            if ((pilha2.peek() + pilha3.peek()) % 3 > 0) {
+                return 1;
+            } else {
+                pilha2.pop();
+                pilha3.pop();
+                return resolucao();
+            }
+        } else if (pilha2.empty()) {
+            if ((pilha1.peek() + pilha3.peek()) % 3 > 0) {
+                return 1;
+            } else {
+                pilha1.pop();
+                pilha3.pop();
+                return resolucao();
+            }
+        } else if (pilha3.empty()) {
+            if ((pilha2.peek() + pilha1.peek()) % 3 > 0) {
+                return 1;
+            } else {
+                pilha2.pop();
+                pilha1.pop();
+                return resolucao();
+            }
+        } else {
+            if ((pilha1.peek() + pilha2.peek() + pilha3.peek()) % 3 == 0) {
+                pilha1.pop();
+                pilha2.pop();
+                pilha3.pop();
+                return resolucao();
+            }
+
+            int num1 = pilha1.peek();
+            int num2 = pilha2.peek();
+            int num3 = pilha3.peek();
+
+            boolean pode12 = false, pode13 = false, pode23 = false;
+
+            if ((num1 + num2) % 3 == 0) {
+                pilha1.pop();
+                pilha2.pop();
+                pode12 = resolucao() == 0;
+                pilha1.push(num1);
+                pilha2.push(num2);
+            } else if ((num2 + num3) % 3 == 0) {
+                pilha2.pop();
+                pilha3.pop();
+                pode23 = resolucao() == 0;
+                pilha2.push(num2);
+                pilha3.push(num3);
+            } else if ((num1 + num3) % 3 == 0) {
+                pilha1.pop();
+                pilha3.pop();
+                pode13 = resolucao() == 0;
+                pilha1.push(num1);
+                pilha3.push(num3);
+            }
+
+            return (pode12 || pode13 || pode23) ? 0 : 1;
+        }
+
+    }
+
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int n = s.nextInt();
-        int[] pilha1 = new int[n];
-        int[] pilha2 = new int[n];
-        int[] pilha3 = new int[n];
-        while (n != 0) {
-            for (int i = n-1; i >= 0; i++) {
-                pilha1[i] = (s.nextInt());
-                pilha2[i] = (s.nextInt());
-                pilha3[i] = (s.nextInt());
-            }
 
+        StringBuilder sb = new StringBuilder();
+
+        while (n != 0) {
+            pilha1.clear();
+            pilha2.clear();
+            pilha3.clear();
+            int somatorio = 0;
+            for (int i = 0; i < n; i++) {
+                int num = s.nextInt();
+                somatorio += num % 3;
+                if (num % 3 > 0) {
+                    pilha1.push(num);
+                }
+                num = s.nextInt();
+                somatorio += num % 3;
+                if (num % 3 > 0) {
+                    pilha2.push(num);
+                }
+                num = s.nextInt();
+                somatorio += num % 3;
+                if (num % 3 > 0) {
+                    pilha3.push(num);
+                }
+            }
+            if (somatorio % 3 == 0) {
+                sb.append(0).append("\n");
+            } else {
+                sb.append(resolucao()).append("\n");
+            }
             n = s.nextInt();
         }
+        System.out.println(sb.toString());
     }
 
 }
