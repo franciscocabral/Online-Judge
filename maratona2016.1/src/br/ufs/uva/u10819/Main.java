97f302fbfd0c7d9a4f0f3a5aa297e725c9589dfe
franciscocabralfilho@gmail.com <>
2016-09-09 12:09:10 -0300

    questão bugada[2]

--- maratona2016.1/src/br/ufs/uva/u10819/Main.java
+++ maratona2016.1/src/br/ufs/uva/u10819/Main.java
@@ -5,57 +5,56 @@
  */
 package br.ufs.uva.u10819;
 
-import java.util.HashMap;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-    public static int[][] tot = new int[10201][101];
-    public static int pesotot = 0;
-    
 
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
 
         while (s.hasNext()) {
             int qtd = s.nextInt();
             int itens = s.nextInt();
-            
-            HashMap<Integer, Integer> pesos = new HashMap<Integer, Integer>();
-            int max = 0;
-            for(int i=0; i < itens; i++){
+
+            int[] preco = new int[itens];
+            int[] favour = new int[itens];
+
+            for (int i = 0; i < itens; i++) {
                 int x = s.nextInt();
                 int y = s.nextInt();
-                if(y > max) max = y;
-                pesos.put(x, y);
+                preco[i] = x;
+                favour[i] = y;
             }
-            tot = new int[10201][101];
-            pesotot = 0;
-            mochila(pesos,qtd,max);
-            System.out.println(pesotot);
-        }
-    }
-    
-    public static void mochila(HashMap<Integer, Integer> pesos,int n, int k){
-        
-        tot[0][0] = 1;
-        for(int i = 0; i < k; i++)
-            tot[0][i] = 0;
-        for(int i = 1; i < n; i++){
-            int peso = -1;
-            if(pesos.containsKey(i)){
-                peso = pesos.get(i);
-            }
-            for(int j = 0; j < k; j++){
-                tot[i][j] = 0;
-                
-                if(tot[i-1][j] >= 1){
-                    tot[i][j] = 1;
-                }else if(j-peso >= 0 && tot[i-1][j-peso] >= 1){
-                    tot[i][j] = 2;
-                    pesotot += peso;
+
+            int qtd200 = (qtd > 1800) ? qtd + 200 : qtd;
+
+            Integer[] todosPrecos = new Integer[qtd200 + 1];
+            Arrays.fill(todosPrecos, 0);
+
+            int maiorFlavor = 0;
+            for (int i = 0; i < itens; i++) {
+                for (int j = qtd200; j >= preco[i]; j--) {
+                    if (todosPrecos[j - preco[i]] > 0 || j == preco[i]) {
+                        todosPrecos[j] = Math.max(todosPrecos[j - preco[i]] + favour[i], todosPrecos[j]);
+                        maiorFlavor = Math.max(maiorFlavor, todosPrecos[j]);
+                    }
                 }
             }
+
+            if (qtd200 >= 1801 && qtd <= 2000) {
+                maiorFlavor = 0;
+                for (int j = qtd200; j >= 0; j++) {
+                    maiorFlavor = Math.max(maiorFlavor, todosPrecos[j]);
+                    if(j == 2001 && maiorFlavor > 0) break;
+                } 
+            }
+
+            sb.append(maiorFlavor).append("\n");
+            //System.out.println(maiorFlavor);
         }
-        
+        System.out.print(sb);
     }
+
 }
